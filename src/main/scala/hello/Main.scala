d1683095f73884fcd1a465f8cc08749c1ba148c0
Franco Sangiacomo <franconecat@gmail.com>
2024-05-10 22:47:06 +0200

    First functional example

--- src/main/scala/hello/Main.scala
+++ src/main/scala/hello/Main.scala
@@ -3,16 +3,16 @@ package hello
 import cats.*
 import cats.effect.*
 import cats.effect.std.Dispatcher
-import fs2.*
 import cats.implicits.*
+import fs2.*
 import fs2.concurrent.Channel
 
 import java.awt
 import java.awt.event.WindowAdapter
 import javax.swing.{SwingUtilities, WindowConstants}
 import scala.concurrent.ExecutionContext
-import scala.swing.event.*
 import scala.swing.*
+import scala.swing.event.*
 
 private object AwtEventDispatchEC extends ExecutionContext {
   def execute(r: Runnable): Unit = SwingUtilities.invokeLater(r)
@@ -21,106 +21,96 @@ private object AwtEventDispatchEC extends ExecutionContext {
 
 object App extends IOApp.Simple {
 
-  private case class UIApp(
-      dispatch: Action => Unit
-  ) extends SimpleSwingApplication {
-
+  case class Counter(dispatch: Action => Unit) extends FlowPanel {
     val countLabel = new Label("0")
 
     def onUpdate(state: State): Unit = {
-      state match {
-        case AppState(counter) => countLabel.text = s"$counter"
-      }
-
-      println("onUpdate")
+      countLabel.text = state.counter.toString
     }
 
-    def top: Frame = new MainFrame {
-      title = "Ignored"
-      contents = new FlowPanel {
-        contents += countLabel
-        contents += new Button {
-          text = "Add 1"
-          reactions += { case event.ButtonClicked(_) =>
-            dispatch(Increase())
-          }
-        }
-        contents += new Button {
-          text = "Subtract 1"
-          reactions += { case event.ButtonClicked(_) =>
-            dispatch(Decrease())
-          }
-        }
+    contents += countLabel
+    contents += new Button {
+      text = "Add 1"
+      reactions += { case event.ButtonClicked(_) =>
+        dispatch(Increase())
       }
-      centerOnScreen()
+    }
+    contents += new Button {
+      text = "Subtract 1"
+      reactions += { case event.ButtonClicked(_) =>
+        dispatch(Decrease())
+      }
     }
   }
 
-  private def ui(ui: UIApp): IO[Unit] = IO.async_[Unit] { cb =>
-    ui.top.peer.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE)
-    ui.top.peer.addWindowListener(new WindowAdapter() {
-      override def windowClosing(e: awt.event.WindowEvent): Unit = {
-        println("Window closing.")
-        cb(Right(()))
-      }
-    })
+  private def ui(content: Counter): IO[Unit] = IO.async_[Unit] { cb =>
+    val app = new SimpleSwingApplication {
+      def top: Frame = new MainFrame {
+        peer.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE)
+        peer.addWindowListener(new WindowAdapter() {
+          override def windowClosing(e: awt.event.WindowEvent): Unit = {
+            println("Window closing.")
+            cb(Right(()))
+          }
+        })
 
-    Thread.setDefaultUncaughtExceptionHandler(
-      new Thread.UncaughtExceptionHandler() {
-        override def uncaughtException(t: Thread, e: Throwable): Unit = {
-          cb(Left(e))
-        }
+        Thread.setDefaultUncaughtExceptionHandler(
+          new Thread.UncaughtExceptionHandler() {
+            override def uncaughtException(t: Thread, e: Throwable): Unit = {
+              cb(Left(e))
+            }
+          }
+        )
+
+        title = "Ignored"
+        contents = content
+        centerOnScreen()
       }
-    )
+    }
 
     // Internally posts to AWT-EventQueue thread
-    ui.main(Array.empty)
+    app.main(Array.empty)
   }
 
-  trait State
-  private final case class AppState(counter: Int) extends State
+  final case class State(counter: Int)
 
   trait Action
   final case class Increase() extends Action
   final case class Decrease() extends Action
 
+  def increase(state: State): State = state.copy(counter = state.counter + 1)
+
+  def decrease(state: State): State = state.copy(counter = state.counter - 1)
+
   override def run: IO[Unit] = {
     (
       Channel.unbounded[IO, Action].toResource,
-      Ref.of[IO, State](AppState(0)).toResource,
+      Ref.of[IO, State](State(0)).toResource,
       Dispatcher.sequential[IO](await = true)
     ).tupled.use { case (actions, ref, dispatcher) =>
       val dispatch: Action => Unit = it => {
         dispatcher.unsafeRunAndForget(actions.send(it))
       }
 
-      val uiApp = UIApp(dispatch)
+      val counter = Counter(dispatch)
 
-      def increase(state: State): State = {
-        state match {
-          case AppState(counter) => AppState(counter + 1)
+      actions.stream
+        .evalTap { action => IO.println(s"Action $action") }
+        .collect {
+          case Increase() => increase(_)
+          case Decrease() => decrease(_)
         }
-      }
-
-      // This will update a state and send it through the states stream.
-      val backgroundProcess =
-        actions.stream
-          .evalTap { action => IO.println(s"Action $action") }
-          .evalMap { action => IO.pure(action).product(ref.get) }
-          .collect { case (action, state) =>
-            action match {
-              case Increase() => increase(state)
-            }
-          }
-          .evalMap { state => ref.set(state) *> IO(uiApp.onUpdate(state)) }
-          .compile
-          .drain
-
-      backgroundProcess.background.surround {
-        IO.println("Starting ui ...") >>
-          ui(uiApp) >> // The ui will receive a stream of states.
-          IO.println("...ui closed")
-      }
+        .evalMap(handler =>
+          ref
+            .updateAndGet(handler)
+            .flatMap(state => IO(counter.onUpdate(state)))
+        )
+        .compile
+        .drain
+        .background
+        .surround {
+          ui(counter)
+        }
     }
   }
 
